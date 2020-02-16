8ec91df3a879db0ddde6160ca1dea0054445a78e
yebin <1074871198@qq.com>
2020-02-16 09:48:00 +0800

    采用ajax提交代码

--- flaskr/proDetailBlueprint.py
+++ flaskr/proDetailBlueprint.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app,jsonify
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -10,40 +10,52 @@ bp = Blueprint('proDetail', __name__, url_prefix='/proDetail')
 
 from . import MysqlUtils
 
-@bp.route("/problemDetail/<proNo>", methods=('GET', 'POST'))
+@bp.route("/problemDetail/<proNo>", methods=['GET'])
 def problemDetail(proNo):
     languages = getLanguages()
     problemInfo = getProblemInfo(proNo)
-    if request.method == 'POST':
-        if session.get('id_user') is None:
-            flash('You need to log in first!','danger')
-            return render_template("proDetail/oneProblem.html",languages = languages,problemInfo=problemInfo)
+    g.proNo = proNo
+    return render_template("proDetail/oneProblem.html",languages = languages,problemInfo=problemInfo)
+
+@bp.route("/submitCode",methods=['POST'])
+def submitCode():
+    error = None
+    if session.get('id_user') is None:
+        error = 'You need to log in first!'
+    if error is None:
         id_user = session.get('id_user');
-        inputCode = request.form["inputCode"]
+        inputCode = request.form.get("inputCode")
         if inputCode is None or inputCode == '':
-            flash('Your answer is empty!','danger')
-            return render_template("proDetail/oneProblem.html",languages = languages,problemInfo=problemInfo)
-        selectLanguage = request.form["selectLanguage"]
+            error = 'Your answer is empty!'
+    if error is None:
+        selectLanguage = request.form.get("selectLanguage")
         id_language = -1;
-        error = None
+        languages = getLanguages()
         for language in languages:
             if language['monaco_editor_val'] == selectLanguage:
                 id_language = language["id_language"]
                 break;
         if id_language != -1:
             try:
+                proNo = request.form.get("proNo")
                 db = MysqlUtils.MyPyMysqlPool()
                 sql = 'INSERT INTO solution (id_user,id_contest_problem,id_language,submit_content) VALUES (\'{}\',\'{}\',\'{}\',\'{}\')'\
                     .format(id_user, proNo,id_language,inputCode)
                 db.insert(sql)
                 flash('Answer submitted successfully!','success')
+                return jsonify({
+                    "result":"success"
+                })
             except:
                 error = "user:{},problemNo:{}. submit answer failure".format(session.get("username"),proNo)
                 current_app.logger.error(error)
             finally:
                 db.dispose()
-    return render_template("proDetail/oneProblem.html",languages = languages,problemInfo=problemInfo)
-
+    flash(error,"danger")
+    return jsonify({
+        "result":"failure",
+        "error":error
+    })
 def getLanguages():
     db = MysqlUtils.MyPyMysqlPool()
     sql = "SELECT id_language,name_language,monaco_editor_val FROM pro_language;"
