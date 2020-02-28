8d884dbdeee61009031814608f553eb22f51d819
yebin <1074871198@qq.com>
2020-02-28 09:20:25 +0800

    添加后台用户管理界面

--- flaskr/adminBlueprint.py
+++ flaskr/adminBlueprint.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for,session,jsonify
+    Blueprint, flash, g, redirect, render_template, request, url_for,session,jsonify,current_app
 )
 from werkzeug.exceptions import abort
 from werkzeug.security import check_password_hash
@@ -36,9 +36,10 @@ def login():
                 error = 'Incorrect password.'
 
         if error is None:
-            session['id_user_admin'] = user['id_user']
-            session["username_admin"] = user['username']
-            session['password_admin'] = user['password']
+            session['id_user'] = user['id_user']
+            session["username"] = user['username']
+            session['password'] = user['password']
+            session["is_admin"] = True
             db.dispose()
             flash('Admin Login successfully!','success')
             return jsonify({
@@ -51,6 +52,8 @@ def login():
             "describe":error
         })
     else:
+        if session.get('is_admin') is not None:
+            return redirect(url_for('admin.index'))
         return render_template("admin/adminLogin.html")
 
 @bp.route("/index")
@@ -58,7 +61,45 @@ def index():
     judgeAdmin()
     return render_template("admin/adminBase.html")
 
+@bp.route("/dashBoard",methods=["POST"])
+def dashBoard():
+    return render_template("admin/dashBoard.html")
+
+@bp.route("/management",methods=["POST"])
+def management():
+    return render_template("admin/management.html")
+
+@bp.route("/logs",methods=["POST"])
+def logs():
+    return render_template("admin/logs.html")
+
+@bp.route("/users",methods=["POST"])
+def users():
+    db = MysqlUtils.MyPyMysqlPool()
+    userList = getUserList(db)
+    db.dispose()
+    return render_template("admin/users.html",userList=userList)
+
+@bp.route("/problems",methods=["POST"])
+def problems():
+    return render_template("admin/problems.html")
+
+@bp.route("/contests",methods=["POST"])
+def contests():
+    return render_template("admin/contests.html")
+
+def getUserList(db):
+    sql = "select id_user,username,password from user"
+    userList = None
+    try:
+        userList = db.get_all(sql)
+    except:
+        current_app.logger.error("get user list failure !")
+    if userList is False or userList is None:
+        return []
+    return userList
+
 def judgeAdmin():
-    if session.get('id_user_admin') is None:
+    if session.get('is_admin') is None:
         flash("Please log in first.",'info')
         return redirect(url_for('admin.login'))
